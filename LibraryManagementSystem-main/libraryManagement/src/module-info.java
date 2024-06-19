d61418930f8e923bfa52f5d73c624227bd9ce9e2
IT Compliance <compliance@amalitech.org>
2024-06-19 08:08:25 -0700

    library management guinea pig

---
+++ LibraryManagementSystem-main/libraryManagement/src/module-info.java
@@ -0,0 +1,14 @@
+module LibraryManagementSystem.main {
+    requires javafx.base;
+    requires  javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.web;
+    requires javafx.swing;
+    requires java.sql;
+    requires fontawesomefx;
+
+    opens librarymanagement;
+
+}
